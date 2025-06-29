44b86ce4f874d9fe3619fa937bd3b8dfb9f6f6e4
ComeONDuDE18 <vibilous6@gmail.com>
2025-06-29 17:24:30 +0200

    VoteOption

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
 import css from './App.module.css';
-import CafeInfo from '../CafeInfo.tsx';
+import CafeInfo from '../CafeInfo/CafeInfo.tsx';
 import { useState } from 'react';
 import type { VoteType, Votes } from '../../types/votes.ts';
-
+import VoteOptions from '../VoteOptions/VoteOptions.tsx';
 
 export default function App() {
   const [votes, setVotes] = useState<Votes>({
@@ -11,16 +11,35 @@ export default function App() {
     bad: 0
   });
 
+  
   const handleVote = (type: VoteType) => {
+    setVotes(prevVotes => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1
+    }));
+  };
+
+  
+  const resetVotes = () => {
     setVotes({
-      ...votes,
-      [type]: votes[type] + 1
+      good: 0,
+      neutral: 0,
+      bad: 0
     });
   };
+
+  
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const isReset = totalVotes !== 0;
 
   return (
     <div className={css.app}>
       <CafeInfo />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={isReset}
+      />
     </div>
   );
 }
