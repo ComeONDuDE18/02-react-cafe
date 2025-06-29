d72ba8faa26c4e082086efdf2858df2da9663372
ComeONDuDE18 <vibilous6@gmail.com>
2025-06-29 14:40:16 +0200

    UpdateFolder

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import css from './App.module.css';
-import CafeInfo from './CafeInfo.tsx';
+import CafeInfo from '../CafeInfo.tsx';
 import { useState } from 'react';
-import type { VoteType, Votes } from '../types/votes.ts';
+import type { VoteType, Votes } from '../../types/votes.ts';
 
 
 export default function App() {
